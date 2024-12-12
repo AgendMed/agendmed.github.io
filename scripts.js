aca5deab6b46b373a956357c8b764ba3e4c0d61e
JSantiagoSL <jsslucio@alu.ufc.br>
2024-12-12 13:47:40 -0300

    adição dos arquivos; modificação do 'service-worker.js'

---
+++ scripts.js
@@ -0,0 +1,7 @@
+// Scripts de cadastro
+var nome, email, senha, confirmarSenha;
+nome = document.getElementById('nome');
+email = document.getElementById('emailCadastro');
+senha = document.getElementById('senhaCadastro');
+confirmarSenha = document.getElementById('confirmarSenha');
+
