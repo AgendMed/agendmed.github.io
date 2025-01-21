2cfc2554f116a184d161bb3a28b199a456accacd
Suziane90 <suzianebrandao90@alu.ufc.br>
2025-01-21 14:54:58 -0300

    testando modeloo

---
+++ .history/Paciente/models_20250121144652.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.conf import settings
+from django.core.validators import MinLengthValidator
+
+
+class Paciente(models.Model):
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE, default=some_default_usuario)
+
+    cartao_saude = models.IntegerField(unique=True)
+    data_nascimento = models.DateField(verbose_name="Data de nascimento")
+    condicao_prioritaria = models.CharField(
+        max_length=50,
+        choices=[
+            ('nenhuma', 'Não possui condição'),
+            ('gravidez', 'Gravidez (Gestante)'),
+            ('idoso', 'Idoso (60 anos ou mais)'),
+            ('doencas_cronicas', 'Doenças Crônicas Graves'),
+            ('urgencia', 'Urgências e Emergências'),
+            ('deficiencia', 'Deficiências Físicas ou Mentais'),
+            ('imunossupresso', 'Pacientes Imunossuprimidos'),
+            ('doencas_infectocontagiosas', 'Doenças Infectocontagiosas'),
+        ],
+        default='nenhuma',
+        null=True,
+        blank=True
+    )
+    comprovante = models.FileField(upload_to='comprovantes/', null=True, blank=True)
+
+    def __str__(self):
+        return self.usuario.nomeCompleto
---
+++ .history/users/models_20250121134050.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+class Perfil(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='perfil')
+    cpf = models.CharField(max_length=14, unique=True)
+    telefone = models.CharField(max_length=15, blank=True, null=True)
+    endereco = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
