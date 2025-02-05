2dbeeeb899bc0bfc6867f69f849925bc2367c462
Alana Martinho <alanamartinho@alu.ufc.br>
2025-02-05 07:31:21 -0300

    att

--- Paciente/urls.py
+++ Paciente/urls.py
@@ -8,11 +8,7 @@ urlpatterns = [
     path('cadastro/', views.cadastro_paciente, name='cadastro_paciente'),
     path('login/', views.login_view, name='login'),
     path('index/', views.Index_view, name='index'),
-<<<<<<< HEAD
-]
-=======
     path('pagina/', views.pagina_paciente, name='pagina_paciente'),
     path('editar_perfil/', views.editar_perfil, name='editar_perfil'),
     path('logout/', views.logout_view, name='logout'),  # Atualizando para usar a nova função
 ]
->>>>>>> 3484c7deaab19140dc8b4125039a15ac978c5937
