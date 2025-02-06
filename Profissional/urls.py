f167ed94963a0406e20f62c99859ef5698027d59
JSantiagoSL <116274952+JSantiagoSL@users.noreply.github.com>
2025-02-06 11:40:47 -0300

    Atualizar o urls.py

--- Profissional/urls.py
+++ Profissional/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.cadastro_profissional, name='cadastro_profissional'),
+    path('c', views.cadastro_profissional, name='cadastro_profissional'),
+   path('profissional_home, views.profissional_home, name='profissional_home'),
+
+
 ]
