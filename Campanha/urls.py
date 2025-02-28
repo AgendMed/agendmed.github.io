68eeafb33e2d2fbf0e6325e16493692a04f6b080
Alana Martinho <alanamartinho@alu.ufc.br>
2025-02-28 09:33:20 -0300

    campanha

---
+++ Campanha/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import cadastrar_campanha, listar_campanhas, deletar_campanha
+
+urlpatterns = [
+    path('cadastrar/', cadastrar_campanha, name='cad_Campanha'),
+    path('listar/', listar_campanhas, name='listar_campanhas'),
+    path('deletar/<int:campanha_id>/', deletar_campanha, name='deletar_campanha'),
+]
