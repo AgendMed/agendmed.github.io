d59f502727dda4cf99c62ecb8d545d7f53cca266
Suziane90 <116275100+Suziane90@users.noreply.github.com>
2025-07-23 18:37:40 -0300

    Add dj-database-url to requirements

--- AgendMed/settings.py
+++ AgendMed/settings.py
@@ -1,4 +1,3 @@
-
 import os
 from pathlib import Path
 from dotenv import load_dotenv
@@ -16,9 +15,7 @@ ALLOWED_HOSTS = []
 
 AUTH_USER_MODEL = 'users.Usuario'
 
-
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -34,7 +31,6 @@ INSTALLED_APPS = [
     'rest_framework',
     'users',
     'especialidades',
-
 ]
 
 MIDDLEWARE = [
@@ -67,10 +63,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'AgendMed.wsgi.application'
 
-
 # Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -82,24 +75,12 @@ DATABASES = {
     }
 }
 
-
-
 # Password validation
-# https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
 ]
 
 AUTHENTICATION_BACKENDS = [
@@ -116,33 +97,21 @@ REST_FRAMEWORK = {
     ],
 }
 
-
 LANGUAGE_CODE = 'pt-br'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_TZ = True
 
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-# Configurações de mídia
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-# Configurações de arquivos estáticos
+# Configurações de arquivos estáticos e mídia
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-LOGIN_URL = '/users/login'
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = '/users/login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
--- AgendMed/urls.py
+++ AgendMed/urls.py
@@ -6,7 +6,6 @@ from AgendMed.settings import BASE_DIR
 from django.urls import path, include, re_path
 from django.views.generic import TemplateView
 from django.views.static import serve
-import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -19,13 +18,10 @@ urlpatterns = [
     path('cadastrar/', include('AgendaConsulta.urls')),
     path('users/', include('users.urls', namespace='users')),
     path('campanha/', include('Campanha.urls')),
-    
+
     # Servir o index.html diretamente
     re_path(r'^$', serve, {'path': 'index.html', 'document_root': BASE_DIR}),
 ]
 
-
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
