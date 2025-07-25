bd0d1f69a2667b3ee6540f7bb7bb817377d6d4fe
Suziane90 <116275100+Suziane90@users.noreply.github.com>
2025-07-25 15:45:43 -0300

    teste do banco5

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 
-python manage.py migrate && gunicorn AgendMed.wsgi:application
+gunicorn AgendMed.wsgi:application --bind 0.0.0.0:$PORT
